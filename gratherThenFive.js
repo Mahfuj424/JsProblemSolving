592395464d59a851982d2a37ecaeac44ca87b0a1
mahfuj <mahfujahmad424@gmail.com>
2024-03-23 16:51:25 +0600

    kno

--- gratherThenFive.js
+++ gratherThenFive.js
@@ -1,8 +1,15 @@
-let numbers = [-1, 2, -3, 4, 6, 5, -7, 8, -9, 10];
 
-for(let i=0; i<numbers.length; i++){
-    let element = numbers[i];
-    if(element>5){
-        console.log(element);
+
+function gratherThenFive(numbers){
+    let count = 0;
+    for(let i=0; i<numbers.length; i++){
+        let element = numbers[i];
+        if(element>5){
+            count = count + 1
+        }
     }
+    return count;
 }
+
+const result = gratherThenFive([-1, 2, -3, 4, 6, 5, -7, 8, -9, 10])
+console.log(result);
