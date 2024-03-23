31380f37821a70f33ab1c58ff8801656ceb4a63e
mahfuj <mahfujahmad424@gmail.com>
2024-03-23 16:45:27 +0600

    nothing

---
+++ gratherThenFive.js
@@ -0,0 +1,8 @@
+let numbers = [-1, 2, -3, 4, 6, 5, -7, 8, -9, 10];
+
+for(let i=0; i<numbers.length; i++){
+    let element = numbers[i];
+    if(element>5){
+        console.log(element);
+    }
+}
