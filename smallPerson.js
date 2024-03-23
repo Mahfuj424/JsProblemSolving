228c61f94c97dd2e2e82221e5541a01b9d6b49e7
mahfuj <mahfujahmad424@gmail.com>
2024-03-23 17:59:05 +0600

    mahfuj

---
+++ smallPerson.js
@@ -0,0 +1,16 @@
+function smallPerson(person) {
+  let smallPerson = "";
+  if (person[0].age < person[1].age) {
+    smallPerson = person[0].name;
+  } else {
+    smallPerson = person[1].name;
+  }
+  return smallPerson;
+}
+
+const result = smallPerson(
+  [{ name: "mahfuj", age: 20 },
+  { name: "abdullah", age: 23 }]
+);
+
+console.log(result);
