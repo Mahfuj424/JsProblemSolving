de7d8f17ae541cec436b6719e521e3e1edd45b82
mahfuj <mahfujahmad424@gmail.com>
2024-03-23 17:45:10 +0600

    mahfuj

---
+++ mlToLiter.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+function mileLiterToLiter(mileLiter){
+    const convert = mileLiter / 1000;
+    return convert;
+}
+
+const result = mileLiterToLiter(2000);
+console.log(result);
+
