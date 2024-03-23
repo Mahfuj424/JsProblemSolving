39117d263bff0dc1104cfb5d153dab0803acfd6d
mahfuj <mahfujahmad424@gmail.com>
2024-03-23 16:16:30 +0600

    nothing

--- summetion.js
+++ summetion.js
@@ -1,9 +1,9 @@
-// function summation(n){
-//     let sum = 0;
-//     for(let i=1; i<=n; i++){
-//         sum = sum +i;
-//     }
-//     return sum;
-// }
-// const result = summation(10);
-// console.log(result);
+function summation(n){
+    let sum = 0;
+    for(let i=1; i<=n; i++){
+        sum = sum +i;
+    }
+    return sum;
+}
+const result = summation(10);
+console.log(result);
