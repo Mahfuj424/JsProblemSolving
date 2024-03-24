e912bc76e85f2b3261028aa0fe89b55a85fde757
mahfuj <mahfujahmad424@gmail.com>
2024-03-24 22:18:15 +0600

    nothing

--- smallPerson.js
+++ smallPerson.js
@@ -1,3 +1,19 @@
+// function smallPerson(person) {
+//   let smallPerson = "";
+//   if (person[0].age < person[1].age) {
+//     smallPerson = person[0].name;
+//   } else {
+//     smallPerson = person[1].name;
+//   }
+//   return smallPerson;
+// }
+
+// const result = smallPerson([
+//   { name: "mahfuj", age: 20 },
+//   { name: "abdullah", age: 23 },
+// ]);
+
+// console.log(result);
 function smallPerson(person) {
   let smallPerson = "";
   if (person[0].age < person[1].age) {
@@ -8,9 +24,9 @@ function smallPerson(person) {
   return smallPerson;
 }
 
-const result = smallPerson(
-  [{ name: "mahfuj", age: 20 },
-  { name: "abdullah", age: 23 }]
-);
+const result = smallPerson([
+  { name: "mahfuj", age: 20 },
+  { name: "abdullah", age: 23 },
+]);
 
 console.log(result);
