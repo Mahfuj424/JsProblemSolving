355793cdbeaa4b416908cf50a98cf3a875222ef6
mahfuj <mahfujahmad424@gmail.com>
2024-03-24 22:24:18 +0600

    nothing

--- gratherThenFive.js
+++ gratherThenFive.js
@@ -1,15 +1,15 @@
 
 
-function gratherThenFive(numbers){
-    let count = 0;
-    for(let i=0; i<numbers.length; i++){
-        let element = numbers[i];
-        if(element>5){
-            count = count + 1
-        }
-    }
-    return count;
-}
+// function gratherThenFive(numbers){
+//     let count = 0;
+//     for(let i=0; i<numbers.length; i++){
+//         let element = numbers[i];
+//         if(element>5){
+//             count = count + 1
+//         }
+//     }
+//     return count;
+// }
 
 function gratherThenFive(numbers){
     let count = 0;
