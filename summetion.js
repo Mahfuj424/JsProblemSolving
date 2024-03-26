8f803bc9e56d7236a379ec95164b45c86472364b
mahfuj <mahfujahmad424@gmail.com>
2024-03-26 22:46:28 +0600

    mahfuj

--- summetion.js
+++ summetion.js
@@ -1,3 +1,12 @@
+// function summation(n){
+//     let sum = 0;
+//     for(let i=1; i<=n; i++){
+//         sum = sum +i;
+//     }
+//     return sum;
+// }
+// const result = summation(10);
+// console.log(result);
 function summation(n){
     let sum = 0;
     for(let i=1; i<=n; i++){
